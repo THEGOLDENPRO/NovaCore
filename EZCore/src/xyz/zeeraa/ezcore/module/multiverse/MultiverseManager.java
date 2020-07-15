432263ea307f855e33a0b50bf4f15ca2fc953423
Anton <udenanton@hotmail.com>
2020-07-15 17:44:05 +0200

    Added multiverse manager

---
+++ EZCore/src/xyz/zeeraa/ezcore/module/multiverse/MultiverseManager.java
@@ -0,0 +1,122 @@
+package xyz.zeeraa.ezcore.module.multiverse;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.WeatherChangeEvent;
+
+import xyz.zeeraa.ezcore.module.EZModule;
+
+public class MultiverseManager extends EZModule implements Listener {
+	private HashMap<String, MultiverseWorld> worlds;
+
+	private static MultiverseManager instance;
+	
+	public static MultiverseManager getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public void onLoad() {
+		instance = this;
+		this.worlds = new HashMap<>();
+	}
+	
+	@Override
+	public void onDisable() {
+		unloadAll();
+	}
+
+	@Override
+	public String getName() {
+		return "Multiverse Manager";
+	}
+
+	public HashMap<String, MultiverseWorld> getWorlds() {
+		return worlds;
+	}
+
+	public MultiverseWorld createWorld(WorldOptions options) {
+		WorldCreator worldCreator = new WorldCreator(options.getName());
+
+		if (options.hasSeed()) {
+			worldCreator.seed(options.getSeed());
+		}
+
+		worldCreator.environment(worldCreator.environment());
+
+		worldCreator.generateStructures(options.isGenerateStructures());
+
+		World world = worldCreator.createWorld();
+
+		MultiverseWorld multiverseWorld = new MultiverseWorld(options.getName(), world, options.getUnloadOption(), options.isSaveOnUnload(), options.isLockWeather());
+
+		worlds.put(options.getName(), multiverseWorld);
+
+		return multiverseWorld;
+	}
+
+	public MultiverseWorld cloneWorld(World world, String name, WorldUnloadOption unloadOption) {
+		WorldCreator worldCreator = new WorldCreator(name);
+
+		worldCreator.copy(world);
+
+		World world2 = worldCreator.createWorld();
+
+		MultiverseWorld multiverseWorld = new MultiverseWorld(name, world2, unloadOption, true, false);
+
+		worlds.put(name, multiverseWorld);
+
+		return multiverseWorld;
+	}
+
+	public MultiverseWorld getWorld(String name) {
+		return worlds.get(name);
+	}
+	
+	public MultiverseWorld getWorld(World world) {
+		return worlds.get(world.getName());
+	}
+	
+	public boolean hasWorld(World world) {
+		return hasWorld(world.getName());
+	}
+	
+	public boolean hasWorld(String name) {
+		return worlds.containsKey(name);
+	}
+
+	public boolean unload(String name) {
+		if (worlds.containsKey(name)) {
+			return unload(worlds.get(name));
+		}
+
+		return false;
+	}
+
+	public boolean unload(MultiverseWorld multiverseWorld) {
+		Bukkit.getServer().unloadWorld(multiverseWorld.getWorld(), multiverseWorld.isSaveOnUnload());
+		//TODO: check WorldUnloadOption
+		worlds.remove(multiverseWorld.getName());
+		return true;
+	}
+
+	public void unloadAll() {
+		for(String worldName : worlds.keySet()) {
+			unload(worldName);
+		}
+	}
+	
+	@EventHandler
+	public void onWatherChage(WeatherChangeEvent e) {
+		if(hasWorld(e.getWorld())) {
+			if(getWorld(e.getWorld()).isLockWeather()) {
+				e.setCancelled(true);
+			}
+		}
+	}
+}
