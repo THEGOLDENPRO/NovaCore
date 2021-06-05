b6b20df8fd83872cc834548ca91b28a59bcd9fd3
Anton udén <udenanton@hotmail.com>
2021-06-05 20:47:11 +0200

    Added number utils

--- NovaCore-Commons/src/main/java/net/zeeraa/novacore/commons/utils/NumberUtils.java
+++ NovaCore-Commons/src/main/java/net/zeeraa/novacore/commons/utils/NumberUtils.java
@@ -10,4 +10,20 @@ public class NumberUtils {
 	public static boolean isEven(int number) {
 		return number % 2 == 0;
 	}
+
+	/**
+	 * Round a number to the provided precision
+	 * <p>
+	 * Found here https://stackoverflow.com/a/22186845
+	 * 
+	 * @param value     The value to round
+	 * @param precision The precision to use
+	 * @return The rounded result
+	 * @since 1.1
+	 * @author https://stackoverflow.com/users/2984077/jpdymond
+	 */
+	public static double round(double value, int precision) {
+		int scale = (int) Math.pow(10, precision);
+		return (double) Math.round(value * scale) / scale;
+	}
 }
