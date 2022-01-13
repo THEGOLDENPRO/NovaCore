8f07525277963ada0c1f048b2ff4d2a3ab9cc379
Anton udén <udenanton@hotmail.com>
2022-01-13 11:58:38 +0100

    Add features to vector area

--- NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/utils/VectorArea.java
+++ NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/utils/VectorArea.java
@@ -3,12 +3,28 @@ package net.zeeraa.novacore.spigot.utils;
 import java.util.Random;
 
 import org.bukkit.util.Vector;
+import org.json.JSONObject;
 
 import net.zeeraa.novacore.commons.utils.RandomGenerator;
 
 public class VectorArea {
 	protected Vector position1;
 	protected Vector position2;
+
+	/**
+	 * Create a {@link VectorArea} from a {@link JSONObject}. The {@link JSONObject}
+	 * needs to have the following properties: x1, y1, z1, x2, y2, z2
+	 * 
+	 * @since 1.1
+	 * @param json Input {@link JSONObject}
+	 * @return the {@link VectorArea} loaded from json
+	 */
+	public static VectorArea fromJSON(JSONObject json) {
+		Vector p1 = new Vector(json.getDouble("x1"), json.getDouble("y1"), json.getDouble("z1"));
+		Vector p2 = new Vector(json.getDouble("x2"), json.getDouble("y2"), json.getDouble("z2"));
+
+		return new VectorArea(p1, p2);
+	}
 
 	public VectorArea(Vector position1, Vector position2) {
 		this.position1 = new Vector((position1.getX() < position2.getX() ? position1.getX() : position2.getX()), (position1.getY() < position2.getY() ? position1.getY() : position2.getY()), (position1.getZ() < position2.getZ() ? position1.getZ() : position2.getZ()));
@@ -28,7 +44,8 @@ public class VectorArea {
 	}
 
 	/**
-	 * Check if a vectors block position is inside this area
+	 * Check if a vectors block position is inside this area. To check location use
+	 * {@link VectorArea#isInside(Vector)}
 	 * 
 	 * @param vector The vector to check
 	 * @return <code>true</code> if the vectors block location is inside this area
@@ -45,6 +62,38 @@ public class VectorArea {
 		int x = vector.getBlockX();
 		int y = vector.getBlockY();
 		int z = vector.getBlockZ();
+
+		if (x >= x1 && x <= x2) {
+			if (y >= y1 && y <= y2) {
+				if (z >= z1 && z <= z2) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if a vectors position is inside this area. To check block location use
+	 * {@link VectorArea#isInsideBlock(Vector)}
+	 * 
+	 * @since 1.1
+	 * @param vector The vector to check
+	 * @return <code>true</code> if the vectors location is inside this area
+	 */
+	public boolean isInside(Vector vector) {
+		double x1 = position1.getX();
+		double y1 = position1.getY();
+		double z1 = position1.getZ();
+
+		double x2 = position2.getX();
+		double y2 = position2.getY();
+		double z2 = position2.getZ();
+
+		double x = vector.getX();
+		double y = vector.getY();
+		double z = vector.getZ();
 
 		if (x >= x1 && x <= x2) {
 			if (y >= y1 && y <= y2) {
