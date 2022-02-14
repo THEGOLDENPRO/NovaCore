3a257d5a36d657d3c47da17164ca5d913576c369
Anton udén <udenanton@hotmail.com>
2022-02-14 10:48:47 +0100

    Update LootTableLoaderV1Legacy.java

--- NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/loottable/loottables/V1/LootTableLoaderV1Legacy.java
+++ NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/loottable/loottables/V1/LootTableLoaderV1Legacy.java
@@ -1,5 +1,12 @@
 package net.zeeraa.novacore.spigot.loottable.loottables.V1;
 
+/**
+ * At one point the V1 loot table was called EZLootTableV1 so to maintain legacy
+ * support we just create a separate version of {@link LootTableLoaderV1} and
+ * change the name to EZLootTableV1
+ * 
+ * @author Zeeraa
+ */
 public class LootTableLoaderV1Legacy extends LootTableLoaderV1 {
 	@Override
 	public String getLoaderName() {
