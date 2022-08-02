36e5d26b66d639f01b6e40195c6d4aa2e689a850
Anton udén <udenanton@hotmail.com>
2022-08-02 12:27:06 +0200

    Add 1.19 enum

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/NovaCoreGameVersion.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/NovaCoreGameVersion.java
@@ -1,7 +1,7 @@
 package net.zeeraa.novacore.spigot.abstraction.enums;
 
 public enum NovaCoreGameVersion {
-	V_1_8(100), V_1_12(1000), V_1_16(2000), V_1_17(2100), V_1_18(2200), V_1_18_R2(2300);
+	V_1_8(100), V_1_12(1000), V_1_16(2000), V_1_17(2100), V_1_18(2200), V_1_18_R2(2300), V_1_19_R1(2400);
 
 	private int versionId;
 
