f5f68abea9f3a59b33d8388ab8a72ab4d7346e5e
Anton udén <udenanton@hotmail.com>
2022-10-13 19:34:59 +0200

    Create RandomProvidedNumber.java

---
+++ NovaCore-Commons/src/main/java/net/zeeraa/novacore/commons/utils/RandomProvidedNumber.java
@@ -0,0 +1,29 @@
+package net.zeeraa.novacore.commons.utils;
+
+import java.util.Random;
+
+public class RandomProvidedNumber {
+	public static final int randomProvidedInt(Random random, int... numbers) {
+		return numbers[random.nextInt(numbers.length)];
+	}
+
+	public static final int randomProvidedInt(int... numbers) {
+		return RandomProvidedNumber.randomProvidedInt(new Random(), numbers);
+	}
+	
+	public static final double randomProvidedDouble(Random random, double... numbers) {
+		return numbers[random.nextInt(numbers.length)];
+	}
+
+	public static final double randomProvidedDouble(double... numbers) {
+		return RandomProvidedNumber.randomProvidedDouble(new Random(), numbers);
+	}
+	
+	public static final float randomProvidedFloat(Random random, float... numbers) {
+		return numbers[random.nextInt(numbers.length)];
+	}
+
+	public static final float randomProvidedFloat(float... numbers) {
+		return RandomProvidedNumber.randomProvidedFloat(new Random(), numbers);
+	}
+}
