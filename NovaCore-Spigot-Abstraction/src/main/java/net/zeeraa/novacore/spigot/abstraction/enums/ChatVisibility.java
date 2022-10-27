5d233c0d398cb7739d55a90091f1757adba5cb13
BrunoGamerGH <bruno.manarino@gmail.com>
2022-10-27 18:26:45 -0300

    Update ChatVisibility.java

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/ChatVisibility.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/ChatVisibility.java
@@ -1,5 +1,29 @@
 package net.zeeraa.novacore.spigot.abstraction.enums;
 
 public enum ChatVisibility {
-	FULL, SYSTEM, HIDDEN
+	FULL("a"), SYSTEM("b"), HIDDEN("c");
+	
+	private final String nmsVal;
+	
+	
+	ChatVisibility(String nmsVal) {
+		this.nmsVal = nmsVal;
+	}
+	
+	public String getNmsVal() {
+		return nmsVal;
+	}
+	
+	public static ChatVisibility fromNMS(String nmsVal) {
+		ChatVisibility cv = null;
+		for (ChatVisibility chatVisibility : ChatVisibility.values()) {
+			if (chatVisibility.getNmsVal().equals(nmsVal)) {
+				cv = chatVisibility;
+				break;
+			}
+		}
+		return cv;
+	}
+
+
 }
