e9645c0f8998a0585334ee123a48c88976f90743
Anton udén <udenanton@hotmail.com>
2022-12-20 15:53:34 +0100

    deprecate 1.17

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/NovaCoreGameVersion.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/enums/NovaCoreGameVersion.java
@@ -1,7 +1,18 @@
 package net.zeeraa.novacore.spigot.abstraction.enums;
 
 public enum NovaCoreGameVersion {
-	V_1_8(100), V_1_12(1000), V_1_16(2000), V_1_17(2100), V_1_18(2200), V_1_18_R2(2300), V_1_19_R1(2400), V_1_19_R2(2500);
+	V_1_8(100),
+	V_1_12(1000),
+	V_1_16(2000),
+	/**
+	 * Deprecated: This version of NovaCore is no longer supported
+	 */
+	@Deprecated
+	V_1_17(2100),
+	V_1_18(2200),
+	V_1_18_R2(2300),
+	V_1_19_R1(2400),
+	V_1_19_R2(2500);
 
 	private int versionId;
 
