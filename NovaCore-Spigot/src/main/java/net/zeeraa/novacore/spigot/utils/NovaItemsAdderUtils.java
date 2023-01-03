656b49964ae0bd07f1eb1fd6d3d313a56a645e77
Anton udén <udenanton@hotmail.com>
2023-01-03 18:14:11 +0100

    Update NovaItemsAdderUtils.java

--- NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/utils/NovaItemsAdderUtils.java
+++ NovaCore-Spigot/src/main/java/net/zeeraa/novacore/spigot/utils/NovaItemsAdderUtils.java
@@ -3,10 +3,15 @@ package net.zeeraa.novacore.spigot.utils;
 import org.bukkit.inventory.ItemStack;
 
 import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
 
 public class NovaItemsAdderUtils {
 	public static ItemStack getItemStack(String namespace) {
 		CustomStack stack = CustomStack.getInstance(namespace);
 		return stack.getItemStack();
+	}
+
+	public static String getFontImage(String namespace) {
+		return new FontImageWrapper(namespace).toString();
 	}
 }
