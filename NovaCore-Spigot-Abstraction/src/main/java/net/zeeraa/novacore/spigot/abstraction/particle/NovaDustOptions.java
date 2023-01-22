040fd6fc2af8c0c732b890b1fe8be53a5575b60b
Anton udén <udenanton@hotmail.com>
2023-01-22 19:12:04 +0100

    Update NovaDustOptions.java

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/particle/NovaDustOptions.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/particle/NovaDustOptions.java
@@ -9,9 +9,27 @@ import org.bukkit.Color;
  * @author Zeeraa
  */
 public class NovaDustOptions {
-	private Color color;
-	private java.awt.Color awtColor;
-	private float size;
+	public static final NovaDustOptions AQUA = new NovaDustOptions(Color.AQUA);
+	public static final NovaDustOptions BLACK = new NovaDustOptions(Color.BLACK);
+	public static final NovaDustOptions BLUE = new NovaDustOptions(Color.BLUE);
+	public static final NovaDustOptions FUCHSIA = new NovaDustOptions(Color.FUCHSIA);
+	public static final NovaDustOptions GRAY = new NovaDustOptions(Color.GRAY);
+	public static final NovaDustOptions GREEN = new NovaDustOptions(Color.GREEN);
+	public static final NovaDustOptions LIME = new NovaDustOptions(Color.LIME);
+	public static final NovaDustOptions MAROON = new NovaDustOptions(Color.MAROON);
+	public static final NovaDustOptions NAVY = new NovaDustOptions(Color.NAVY);
+	public static final NovaDustOptions OLIVE = new NovaDustOptions(Color.OLIVE);
+	public static final NovaDustOptions ORANGE = new NovaDustOptions(Color.ORANGE);
+	public static final NovaDustOptions PURPLE = new NovaDustOptions(Color.PURPLE);
+	public static final NovaDustOptions RED = new NovaDustOptions(Color.RED);
+	public static final NovaDustOptions SILVER = new NovaDustOptions(Color.SILVER);
+	public static final NovaDustOptions TEAL = new NovaDustOptions(Color.TEAL);
+	public static final NovaDustOptions WHITE = new NovaDustOptions(Color.WHITE);
+	public static final NovaDustOptions YELLOW = new NovaDustOptions(Color.YELLOW);
+
+	private final Color color;
+	private final java.awt.Color awtColor;
+	private final float size;
 
 	public NovaDustOptions(java.awt.Color color, float size) {
 		this.color = Color.fromRGB(color.getRed(), color.getGreen(), color.getBlue());
