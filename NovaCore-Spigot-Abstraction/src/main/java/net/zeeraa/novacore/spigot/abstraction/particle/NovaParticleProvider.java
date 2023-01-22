02eaa6689f6ff67db4e68ba7d8a7a7f08f6f79b5
Anton udén <udenanton@hotmail.com>
2023-01-22 18:58:58 +0100

    Update NovaParticleProvider.java

--- NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/particle/NovaParticleProvider.java
+++ NovaCore-Spigot-Abstraction/src/main/java/net/zeeraa/novacore/spigot/abstraction/particle/NovaParticleProvider.java
@@ -7,6 +7,22 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public abstract class NovaParticleProvider {
+	public void showRedstoneParticle(Location location, java.awt.Color color) {
+		this.showRedstoneParticle(location, new NovaDustOptions(color, 1));
+	}
+
+	public void showRedstoneParticle(Location location, java.awt.Color color, Player receiver) {
+		this.showRedstoneParticle(location, new NovaDustOptions(color, 1), receiver);
+	}
+
+	public void showRedstoneParticle(Location location, java.awt.Color color, Collection<Player> receivers) {
+		this.showRedstoneParticle(location, new NovaDustOptions(color, 1), receivers);
+	}
+
+	public void showRedstoneParticle(Location location, java.awt.Color color, Predicate<Player> predicate) {
+		this.showRedstoneParticle(location, new NovaDustOptions(color, 1), predicate);
+	}
+
 	public abstract void showRedstoneParticle(Location location, NovaDustOptions options);
 
 	public abstract void showRedstoneParticle(Location location, NovaDustOptions options, Player receiver);
